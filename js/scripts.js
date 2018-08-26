5a7f86633867e6611417463c522ffbc555a71bcb
LAPTOP-2SRR9CLJ\David Gaylord <davidsart85@yahoo.com>
2018-08-25 22:31:43 -0700

    no message

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi! Welcome to My Portfolio Site")
+
+$(document).ready(function() {
+  $(".project").on("mouseenter", function() {
+    $(this).find(".project-details").fadeIn();
+    });
+  $(".project").on("mouseleave", function() {
+    $(this).find(".project-details").fadeOut();
+    });
+});
